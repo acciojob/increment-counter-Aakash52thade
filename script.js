a29588467280e00ab5d688288d3d0dae09ddc7e9
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-29 06:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const count = document.getElementById("counter");
+let val = parseInt(count.innerHTML); // convert to number
+
+function handleClick() {
+	alert(val);
+	val = val + 1;
+	val.innerHTML = val;
+}
